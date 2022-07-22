7f082a2cad17b5371d6c0956e938fce9e0906c76
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-23 01:55:51 +0430

    add field step to cart

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models import Sum, F
 from django.utils.translation import ugettext_lazy as _
 
 from painless.utils.models.mixins import TimeStampModelMixin
@@ -9,12 +10,23 @@ class Cart(TimeStampModelMixin):
     """
     Cart model
     """
+    STEP_CHOICES = (
+        ('initial', _('Initial')),
+        ('pending', _('Pending')),
+        ('paid', _('Paid'))
+    )
+    step = models.CharField(_('Step'), choices=STEP_CHOICES, max_length=20, default='initial')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     # TODO: should add discount_code field
 
     def __str__(self):
         return '{}'.format(self.user)
+
+    @property
+    def total_price(self):
+        return self.items.aggregate(
+            total=Sum(F('product__final_price') * F('quantity')))['total']
 
     class Meta:
         verbose_name = _('Cart')
@@ -25,7 +37,7 @@ class CartItem(TimeStampModelMixin):
     """
     Cart item model
     """
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
