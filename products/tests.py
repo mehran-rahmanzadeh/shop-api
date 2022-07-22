c8788ebf9202020a562f0958eafee4457d01d9b4
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-23 00:57:44 +0430

    add product api tests

--- products/tests.py
+++ products/tests.py
@@ -1,4 +1,6 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.db import IntegrityError, transaction
+from django.urls import reverse_lazy
 from rest_framework.test import APITestCase
 
 from categories.models import Category
@@ -46,6 +48,7 @@ class TestProducts(APITestCase):
 
     @staticmethod
     def create_category(title, slug, description, parent, order, image):
+        """Create category"""
         return Category.objects.create(
             title=title,
             slug=slug,
@@ -57,6 +60,7 @@ class TestProducts(APITestCase):
 
     @staticmethod
     def create_product(title, slug, base_price, discount_amount, has_discount, quantity, category, description):
+        """Create product"""
         return Product.objects.create(
             title=title,
             slug=slug,
@@ -67,6 +71,24 @@ class TestProducts(APITestCase):
             has_discount=has_discount,
             quantity=quantity
         )
+
+    def assert_product_fields(self, data, product):
+        """Assert product fields
+        :param response: rest client response
+        :param product: product
+        """
+        self.assertEqual(data['title'], product.title)
+        self.assertEqual(data['slug'], product.slug)
+        self.assertEqual(data['description'], product.description)
+        self.assertEqual(float(data['base_price']), product.base_price)
+        self.assertEqual(float(data['discount_amount']), product.discount_amount)
+        self.assertEqual(data['has_discount'], product.has_discount)
+        self.assertEqual(int(data['quantity']), product.quantity)
+        self.assertEqual(data['category']['title'], product.category.title)
+        self.assertEqual(data['category']['slug'], product.category.slug)
+        self.assertEqual(data['category']['description'], product.category.description)
+        self.assertEqual(data['category']['order'], product.category.order)
+        self.assertEqual(data['category']['image'], product.category.image)
 
     def test_create_product_success(self):
         """Test create product"""
@@ -136,3 +158,52 @@ class TestProducts(APITestCase):
         self.assertEqual(self.product.discount_percentage, 10)
         self.product.has_discount = False
         self.assertEqual(self.product.discount_percentage, 0)
+
+    def test_get_product_list(self):
+        """Test get product list"""
+        url = reverse_lazy('product-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assert_product_fields(response.data['results'][0], self.product)
+
+    def test_retrieve_product(self):
+        """Test retrieve product"""
+        url = reverse_lazy('product-detail', kwargs={'slug': self.slug})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assert_product_fields(response.data, self.product)
+
+    def test_filter_product_by_category(self):
+        """Test filter product by category"""
+        url = reverse_lazy('product-list')
+        url += '?category={}'.format(self.category.id)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assert_product_fields(response.data['results'][0], self.product)
+
+    def test_filter_product_by_parent_category(self):
+        """Test filter product by parent category"""
+        child_category = self.create_category(
+            title='Test category 2',
+            slug='test-category-2', order=2,
+            parent=self.category,
+            description='Test description 2',
+            image=None
+        )
+        child_product = self.create_product(
+            title='Test product 2',
+            slug='test-product-2',
+            category=child_category,
+            quantity=10,
+            base_price=100,
+            discount_amount=0,
+            has_discount=False,
+            description='Test description 2'
+        )
+        url = reverse_lazy('product-list')
+        url += '?category={}'.format(self.category.id)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 2)
