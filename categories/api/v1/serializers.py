e09f3adc339145d702f0c5c8232ea7d2816f81af
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-22 22:50:15 +0430

    add category serializer

---
+++ categories/api/v1/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+
+from categories.models import Category
+
+
+class CategorySerializer(ModelSerializer):
+    """Category serializer class"""
+
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'title',
+            'slug',
+            'description',
+            'order',
+            'image'
+        )
+
+    def get_fields(self):
+        fields = super().get_fields()
+        fields['children'] = CategorySerializer(many=True, required=False)
+        return fields
