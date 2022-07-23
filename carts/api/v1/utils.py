5381c85fed3622cf9aef5a58e7438d38c0957dc5
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-23 14:39:03 +0430

    fix cart helper

--- carts/api/v1/utils.py
+++ carts/api/v1/utils.py
@@ -1,7 +1,17 @@
+from carts.models import Cart
+
+
 class CartHelper:
     def __init__(self, *args, **kwargs):
         super(CartHelper, self).__init__(*args, **kwargs)
 
     @classmethod
     def get_current_cart(cls, user):
-        return user.carts.filter(step__in=['initial', 'pending']).first()
+        """Get user current cart
+        :param: user
+        :return: cart
+        """
+        carts = user.carts.filter(step__in=['initial', 'pending'])
+        if carts.exists():
+            return carts.first()
+        return Cart.objects.create(user=user)
