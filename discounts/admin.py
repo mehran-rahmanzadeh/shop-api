3693a8869e33e962fcbb2cfb78c3582a7266d6a5
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-23 13:27:27 +0430

    add discoutn code model and admin

--- discounts/admin.py
+++ discounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from discounts.models import DiscountCode
+
+
+@admin.register(DiscountCode)
+class DiscountCodeAdmin(admin.ModelAdmin):
+    """
+    Discount code admin
+    """
+    list_display = ('code', 'kind', 'percentage', 'amount', 'is_active')
+    list_filter = ('kind', 'is_active')
+    search_fields = ('code', 'used_by__username')
+    save_on_top = True
+    save_as = True
+    actions = ('activate', 'deactivate')
+
+    def activate(self, request, queryset):
+        """Activate selected discount codes"""
+        queryset.update(is_active=True)
+
+    def deactivate(self, request, queryset):
+        """Deactivate selected discount codes"""
+        queryset.update(is_active=False)
+
+    activate.short_description = 'Activate selected discount codes'
+    deactivate.short_description = 'Deactivate selected discount codes'
+
+    def get_queryset(self, request):
+        """Get queryset"""
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('used_by')
--- discounts/models.py
+++ discounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
-# Create your models here.
+from painless.utils.models.mixins import TimeStampModelMixin
+
+
+class DiscountCode(TimeStampModelMixin):
+    """
+    Discount code model
+    kind: (percentage, fixed amount)
+    code: unique code
+    percentage: percentage of discount
+    amount: fixed amount of discount
+    used_by: list of users who used this code
+    is_active: is this code active
+    """
+    KIND_CHOICES = (
+        ('percent', _('Percentage')),
+        ('amount', _('Amount')),
+    )
+    kind = models.CharField(_('Kind'), max_length=10, choices=KIND_CHOICES)
+    code = models.CharField(_('Code'), max_length=255, unique=True)
+    percentage = models.PositiveSmallIntegerField(_('Percentage'), null=True, blank=True)
+    amount = models.DecimalField(_('Amount'), max_digits=10, decimal_places=2, null=True, blank=True)
+    used_by = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, verbose_name=_('Used by'),
+        blank=True, editable=False
+    )
+    is_active = models.BooleanField(_('Is active'), default=True)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = _('Discount code')
+        verbose_name_plural = _('Discount codes')
+        ordering = ('-created',)
