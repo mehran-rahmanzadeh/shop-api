d5a56b29e37f41c06bdb546def0ccf7fe5bd0d0f
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-07-23 13:07:42 +0430

    add jwt urls

--- kernel/urls.py
+++ kernel/urls.py
@@ -21,6 +21,10 @@ from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,8 +48,11 @@ urlpatterns = [
     path('api/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-swagger-ui'),
 
     # API
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include('categories.api.v1.urls')),
     path('api/v1/', include('products.api.v1.urls')),
+    path('api/v1/', include('carts.api.v1.urls')),
 
 ] + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
